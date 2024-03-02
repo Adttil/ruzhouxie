156a02a8b962ebc7f3d7ab8bf7bc1d98888a3714
Adttil <1963096826@qq.com>
2024-03-02 17:55:52 +0800

    edit optimize test.

--- test/optimize.cpp
+++ test/optimize.cpp
@@ -6,41 +6,25 @@
 
 namespace rzx = ruzhouxie;
 
-struct value_t
-{
-    int value;
-    value_t(int value) : value(value){}
-
-    friend value_t operator-(const value_t& l)noexcept
-    {
-        std::puts("operator-(const value_t&);");
-        return value_t{ -l.value };
-    }
-};
-
 int main()
 {
     constexpr auto layout1 = std::array
     {
         std::array{0uz}, std::array{0uz}
     };
-
     constexpr auto layout2 = std::array
     {
         std::array{0uz}, std::array{1uz}, std::array{0uz}, std::array{1uz}
     };
 
-    auto input = std::array{ value_t{ 233 } };
+    auto input = std::array{ 233 };
+    auto neg = [](const auto& x)
+    {
+        std::puts("neg");
+        return -x;
+    };
 
-    std::puts("==================");
-    std::array<value_t, 2> result0 = +(input | rzx::relayout<layout1> | rzx::transform(std::negate<>{}));
-    std::puts("==================");
+    std::array<int, 4> result = +(input | rzx::relayout<layout1> | rzx::transform(neg) |  rzx::relayout<layout2>);
 
-    std::puts("==================");
-    std::array<value_t, 2> result1 = +(input | rzx::transform(std::negate<>{}) | rzx::relayout<layout1>);
-    std::puts("==================");
-
-    std::puts("==================");
-    std::array<value_t, 4> result2 = +(input | rzx::relayout<layout1> | rzx::transform(std::negate<>{}) |  rzx::relayout<layout2>);
-    std::puts("==================");
+    for(int x : result) std::cout << x << ' ';
 }
