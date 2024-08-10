933678d10522ce67820224f481d0fb13d0db31b4
Adttil <1963096826@qq.com>
2024-08-10 11:21:41 +0800

    Temporarily comment out unused content.

--- include/ruzhouxie/general.hpp
+++ include/ruzhouxie/general.hpp
@@ -23,107 +23,107 @@ namespace ruzhouxie
 
     namespace rzx = ::ruzhouxie;
 
-    inline constexpr size_t invalid_index = std::numeric_limits<size_t>::max();
+    //inline constexpr size_t invalid_index = std::numeric_limits<size_t>::max();
 
     struct custom_t{};
 
 	//type without cvref.
-    template<class T>
-    concept pure = std::same_as<T, std::remove_cvref_t<T>>;
+    //template<class T>
+    //concept pure = std::same_as<T, std::remove_cvref_t<T>>;
 
-    template<class T>
-    using purified = std::remove_cvref_t<T>;
+    //template<class T>
+    //using purified = std::remove_cvref_t<T>;
 
-    template<auto fn>
-    using tag_t = purified<decltype(fn)>;
+    //template<auto fn>
+    //using tag_t = purified<decltype(fn)>;
 	
 	//completiable type.
-    template<class T>
-    concept concrete = !std::same_as<T, void> && !std::is_abstract_v<T> && !std::is_unbounded_array_v<T>;
+    //template<class T>
+    //concept concrete = !std::same_as<T, void> && !std::is_abstract_v<T> && !std::is_unbounded_array_v<T>;
 
 	//const object or ref to const object
-    template<class T>
-    concept readonly = std::is_const_v<std::remove_reference_t<T>>;
+    //template<class T>
+    //concept readonly = std::is_const_v<std::remove_reference_t<T>>;
 
     template<class T>
-    concept aggregated = std::is_aggregate_v<purified<T>>;
+    concept aggregated = std::is_aggregate_v<std::remove_cvref_t<T>>;
 
-    template<class T>
-    concept empty = std::is_empty_v<T>;
+    //template<class T>
+    //concept empty = std::is_empty_v<T>;
 
-    template<class T>
-    concept empty_aggregated = std::is_empty_v<T> && std::is_aggregate_v<T>;
+    //template<class T>
+    //concept empty_aggregated = std::is_empty_v<T> && std::is_aggregate_v<T>;
 
-    template<class T>
-    concept inheritable = std::is_class_v<T> && !std::is_final_v<T>;
+    //template<class T>
+    //concept inheritable = std::is_class_v<T> && !std::is_final_v<T>;
 
-    template<auto...>
-    concept constevaluable = true;
+    //template<auto...>
+    //concept constevaluable = true;
 
 	template<class T, class U>
     concept derived_from = std::derived_from<std::remove_cvref_t<T>, std::remove_cvref_t<U>>;
 
-    template<class T, class U>
-    concept is = std::same_as<U, T> || std::derived_from<T, U>;
+    //template<class T, class U>
+    //concept is = std::same_as<U, T> || std::derived_from<T, U>;
 
 	//T is type of arbitrarily adding &, &&, const, volatile to TOriginal.
-    template<class T, class TOriginal>
-    concept specified = is<purified<T>, purified<TOriginal>> &&
-		(!std::is_reference_v<TOriginal> || std::is_reference_v<T>) &&
-		(!std::is_lvalue_reference_v<TOriginal> || std::is_lvalue_reference_v<T>) &&
-		(!readonly<TOriginal> || readonly<T>) &&
-		(!std::is_volatile_v<std::remove_reference_t<TOriginal>> || std::is_volatile_v<std::remove_reference_t<T>>);
+    // template<class T, class TOriginal>
+    // concept specified = is<purified<T>, purified<TOriginal>> &&
+	// 	(!std::is_reference_v<TOriginal> || std::is_reference_v<T>) &&
+	// 	(!std::is_lvalue_reference_v<TOriginal> || std::is_lvalue_reference_v<T>) &&
+	// 	(!readonly<TOriginal> || readonly<T>) &&
+	// 	(!std::is_volatile_v<std::remove_reference_t<TOriginal>> || std::is_volatile_v<std::remove_reference_t<T>>);
 
-    template<class T>
-    concept expiringed = std::is_rvalue_reference_v<T>;
+    // template<class T>
+    // concept expiringed = std::is_rvalue_reference_v<T>;
 
 	// T is type of non-const r-value reference.
-    template<class T>
-    concept proprietary =
-	    std::is_rvalue_reference_v<T>
-		&&
-		!std::is_const_v<std::remove_reference_t<T>>;
+    // template<class T>
+    // concept proprietary =
+	//     std::is_rvalue_reference_v<T>
+	// 	&&
+	// 	!std::is_const_v<std::remove_reference_t<T>>;
 
-    template<class T>
-    T declvalue()noexcept;
+    // template<class T>
+    // T declvalue()noexcept;
 
-    template<size_t I, class...T>
-    using type_at = std::tuple_element_t<I, std::tuple<T...>>;
+    // template<size_t I, class...T>
+    // using type_at = std::tuple_element_t<I, std::tuple<T...>>;
 
 	template<class T, class...U>
 	using fwd_type = std::conditional_t< (... && std::is_rvalue_reference_v<U&&>),
 			    						 std::remove_reference_t<T>&&,
 			    						 std::remove_reference_t<T>& >;
 
-    template<class...T>
-    RUZHOUXIE_INTRINSIC constexpr decltype(auto) fwd(auto&& arg) noexcept
-	{
-		return static_cast<fwd_type<decltype(arg), T...>>(arg);
-	}
+    // template<class...T>
+    // RUZHOUXIE_INTRINSIC constexpr decltype(auto) fwd(auto&& arg) noexcept
+	// {
+	// 	return static_cast<fwd_type<decltype(arg), T...>>(arg);
+	// }
 
-    template<class TBase>
-    RUZHOUXIE_INTRINSIC constexpr TBase& as_base(TBase& arg) noexcept
-	{
-	    return arg;
-	}
+    // template<class TBase>
+    // RUZHOUXIE_INTRINSIC constexpr TBase& as_base(TBase& arg) noexcept
+	// {
+	//     return arg;
+	// }
 
-    template<class TBase>
-    RUZHOUXIE_INTRINSIC constexpr const TBase& as_base(const TBase& arg) noexcept
-	{
-	    return arg;
-	}
+    // template<class TBase>
+    // RUZHOUXIE_INTRINSIC constexpr const TBase& as_base(const TBase& arg) noexcept
+	// {
+	//     return arg;
+	// }
 
-    template<class TBase>
-    RUZHOUXIE_INTRINSIC constexpr TBase&& as_base(TBase&& arg) noexcept
-	{
-	    return std::move(arg);
-	}
+    // template<class TBase>
+    // RUZHOUXIE_INTRINSIC constexpr TBase&& as_base(TBase&& arg) noexcept
+	// {
+	//     return std::move(arg);
+	// }
 
-    template<class TBase>
-    RUZHOUXIE_INTRINSIC constexpr const TBase&& as_base(const TBase&& arg) noexcept
-	{
-	    return std::move(arg);
-	}
+    // template<class TBase>
+    // RUZHOUXIE_INTRINSIC constexpr const TBase&& as_base(const TBase&& arg) noexcept
+	// {
+	//     return std::move(arg);
+	// }
 
     template <class T = bool>
     struct choice_t
@@ -135,42 +135,42 @@ namespace ruzhouxie
 	template<class T, std::convertible_to<bool> B>
 	choice_t(T&&, B&&) -> choice_t<std::decay<T>>;
 
-    template<class T>
-    struct wrapper
-	{
-	    T raw_value;
+    // template<class T>
+    // struct wrapper
+	// {
+	//     T raw_value;
 
-	    constexpr auto&& value(this auto&& self)noexcept
-		{
-		    return FWD(self, raw_value);
-		}
-	};
+	//     constexpr auto&& value(this auto&& self)noexcept
+	// 	{
+	// 	    return FWD(self, raw_value);
+	// 	}
+	// };
 
-	template<class T>
-	wrapper(T&&) -> wrapper<T>;
+	// template<class T>
+	// wrapper(T&&) -> wrapper<T>;
 }
 
 namespace ruzhouxie
 {
 
-    template<size_t I>
-    RUZHOUXIE_INLINE constexpr auto&& arg_at(auto&&...args)noexcept
-	{
-	    return std::get<I>(std::forward_as_tuple(FWD(args)...));
-	}
+    // template<size_t I>
+    // RUZHOUXIE_INLINE constexpr auto&& arg_at(auto&&...args)noexcept
+	// {
+	//     return std::get<I>(std::forward_as_tuple(FWD(args)...));
+	// }
 
-    RUZHOUXIE_INLINE constexpr bool equal(auto&& x, auto&& y) 
-	    noexcept(not requires{ FWD(x) == FWD(y); } || requires{ requires noexcept(FWD(x) == FWD(y)); })
-	{
-	    if constexpr(requires{ FWD(x) == FWD(y); })
-		{
-		    return FWD(x) == FWD(y);
-		}
-	    else
-		{
-		    return false;
-		}
-	}
+    // RUZHOUXIE_INLINE constexpr bool equal(auto&& x, auto&& y) 
+	//     noexcept(not requires{ FWD(x) == FWD(y); } || requires{ requires noexcept(FWD(x) == FWD(y)); })
+	// {
+	//     if constexpr(requires{ FWD(x) == FWD(y); })
+	// 	{
+	// 	    return FWD(x) == FWD(y);
+	// 	}
+	//     else
+	// 	{
+	// 	    return false;
+	// 	}
+	// }
 }
 
 #include "macro_undef.hpp"
--- test/general.cpp
+++ test/general.cpp
@@ -5,98 +5,98 @@
 
 using namespace ruzhouxie;
 
-TEST(specified, same)
-{
-    MAGIC_CHECK(true, specified<int, int>);
-    MAGIC_CHECK(true, specified<const int, int>);
-    MAGIC_CHECK(true, specified<int&, int>);
-    MAGIC_CHECK(true, specified<const int&, int>);
-    MAGIC_CHECK(true, specified<int&&, int>);
-    MAGIC_CHECK(true, specified<const int&&, int>);
+// TEST(specified, same)
+// {
+//     MAGIC_CHECK(true, specified<int, int>);
+//     MAGIC_CHECK(true, specified<const int, int>);
+//     MAGIC_CHECK(true, specified<int&, int>);
+//     MAGIC_CHECK(true, specified<const int&, int>);
+//     MAGIC_CHECK(true, specified<int&&, int>);
+//     MAGIC_CHECK(true, specified<const int&&, int>);
 
-    MAGIC_CHECK(false, specified<int, const int>);
-    MAGIC_CHECK(true, specified<const int, const int>);
-    MAGIC_CHECK(false, specified<int&, const int>);
-    MAGIC_CHECK(true, specified<const int&, const int>);
-    MAGIC_CHECK(false, specified<int&&, const int>);
-    MAGIC_CHECK(true, specified<const int&&, const int>);
+//     MAGIC_CHECK(false, specified<int, const int>);
+//     MAGIC_CHECK(true, specified<const int, const int>);
+//     MAGIC_CHECK(false, specified<int&, const int>);
+//     MAGIC_CHECK(true, specified<const int&, const int>);
+//     MAGIC_CHECK(false, specified<int&&, const int>);
+//     MAGIC_CHECK(true, specified<const int&&, const int>);
 
-    MAGIC_CHECK(false, specified<int, int&>);
-    MAGIC_CHECK(false, specified<const int, int&>);
-    MAGIC_CHECK(true, specified<int&, int&>);
-    MAGIC_CHECK(true, specified<const int&, int&>);
-    MAGIC_CHECK(false, specified<int&&, int&>);
-    MAGIC_CHECK(false, specified<const int&&, int&>);
+//     MAGIC_CHECK(false, specified<int, int&>);
+//     MAGIC_CHECK(false, specified<const int, int&>);
+//     MAGIC_CHECK(true, specified<int&, int&>);
+//     MAGIC_CHECK(true, specified<const int&, int&>);
+//     MAGIC_CHECK(false, specified<int&&, int&>);
+//     MAGIC_CHECK(false, specified<const int&&, int&>);
 
-    MAGIC_CHECK(false, specified<int, const int&>);
-    MAGIC_CHECK(false, specified<const int, const int&>);
-    MAGIC_CHECK(false, specified<int&, const int&>);
-    MAGIC_CHECK(true, specified<const int&, const int&>);
-    MAGIC_CHECK(false, specified<int&&, const int&>);
-    MAGIC_CHECK(false, specified<const int&&, const int&>);
+//     MAGIC_CHECK(false, specified<int, const int&>);
+//     MAGIC_CHECK(false, specified<const int, const int&>);
+//     MAGIC_CHECK(false, specified<int&, const int&>);
+//     MAGIC_CHECK(true, specified<const int&, const int&>);
+//     MAGIC_CHECK(false, specified<int&&, const int&>);
+//     MAGIC_CHECK(false, specified<const int&&, const int&>);
 
-    MAGIC_CHECK(false, specified<int, int&&>);
-    MAGIC_CHECK(false, specified<const int, int&&>);
-    MAGIC_CHECK(true, specified<int&, int&&>);
-    MAGIC_CHECK(true, specified<const int&, int&&>);
-    MAGIC_CHECK(true, specified<int&&, int&&>);
-    MAGIC_CHECK(true, specified<const int&&, int&&>);
+//     MAGIC_CHECK(false, specified<int, int&&>);
+//     MAGIC_CHECK(false, specified<const int, int&&>);
+//     MAGIC_CHECK(true, specified<int&, int&&>);
+//     MAGIC_CHECK(true, specified<const int&, int&&>);
+//     MAGIC_CHECK(true, specified<int&&, int&&>);
+//     MAGIC_CHECK(true, specified<const int&&, int&&>);
 
-    MAGIC_CHECK(false, specified<int, const int&&>);
-    MAGIC_CHECK(false, specified<const int, const int&>);
-    MAGIC_CHECK(false, specified<int&, const int&&>);
-    MAGIC_CHECK(true, specified<const int&, const int&&>);
-    MAGIC_CHECK(false, specified<int&&, const int&&>);
-    MAGIC_CHECK(true, specified<const int&&, const int&&>);
-}
+//     MAGIC_CHECK(false, specified<int, const int&&>);
+//     MAGIC_CHECK(false, specified<const int, const int&>);
+//     MAGIC_CHECK(false, specified<int&, const int&&>);
+//     MAGIC_CHECK(true, specified<const int&, const int&&>);
+//     MAGIC_CHECK(false, specified<int&&, const int&&>);
+//     MAGIC_CHECK(true, specified<const int&&, const int&&>);
+// }
 
-TEST(specified, derived)
-{
-    struct Base{};
-    struct Drived : Base{};
+// TEST(specified, derived)
+// {
+//     struct Base{};
+//     struct Drived : Base{};
 
-    MAGIC_CHECK(true, specified<Drived, Base>);
-    MAGIC_CHECK(true, specified<const Drived, Base>);
-    MAGIC_CHECK(true, specified<Drived&, Base>);
-    MAGIC_CHECK(true, specified<const Drived&, Base>);
-    MAGIC_CHECK(true, specified<Drived&&, Base>);
-    MAGIC_CHECK(true, specified<const Drived&&, Base>);
+//     MAGIC_CHECK(true, specified<Drived, Base>);
+//     MAGIC_CHECK(true, specified<const Drived, Base>);
+//     MAGIC_CHECK(true, specified<Drived&, Base>);
+//     MAGIC_CHECK(true, specified<const Drived&, Base>);
+//     MAGIC_CHECK(true, specified<Drived&&, Base>);
+//     MAGIC_CHECK(true, specified<const Drived&&, Base>);
 
-    MAGIC_CHECK(false, specified<Drived, const Base>);
-    MAGIC_CHECK(true, specified<const Drived, const Base>);
-    MAGIC_CHECK(false, specified<Drived&, const Base>);
-    MAGIC_CHECK(true, specified<const Drived&, const Base>);
-    MAGIC_CHECK(false, specified<Drived&&, const Base>);
-    MAGIC_CHECK(true, specified<const Drived&&, const Base>);
+//     MAGIC_CHECK(false, specified<Drived, const Base>);
+//     MAGIC_CHECK(true, specified<const Drived, const Base>);
+//     MAGIC_CHECK(false, specified<Drived&, const Base>);
+//     MAGIC_CHECK(true, specified<const Drived&, const Base>);
+//     MAGIC_CHECK(false, specified<Drived&&, const Base>);
+//     MAGIC_CHECK(true, specified<const Drived&&, const Base>);
 
-    MAGIC_CHECK(false, specified<Drived, Base&>);
-    MAGIC_CHECK(false, specified<const Drived, Base&>);
-    MAGIC_CHECK(true, specified<Drived&, Base&>);
-    MAGIC_CHECK(true, specified<const Drived&, Base&>);
-    MAGIC_CHECK(false, specified<Drived&&, Base&>);
-    MAGIC_CHECK(false, specified<const Drived&&, Base&>);
+//     MAGIC_CHECK(false, specified<Drived, Base&>);
+//     MAGIC_CHECK(false, specified<const Drived, Base&>);
+//     MAGIC_CHECK(true, specified<Drived&, Base&>);
+//     MAGIC_CHECK(true, specified<const Drived&, Base&>);
+//     MAGIC_CHECK(false, specified<Drived&&, Base&>);
+//     MAGIC_CHECK(false, specified<const Drived&&, Base&>);
 
-    MAGIC_CHECK(false, specified<Drived, const Base&>);
-    MAGIC_CHECK(false, specified<const Drived, const Base&>);
-    MAGIC_CHECK(false, specified<Drived&, const Base&>);
-    MAGIC_CHECK(true, specified<const Drived&, const Base&>);
-    MAGIC_CHECK(false, specified<Drived&&, const Base&>);
-    MAGIC_CHECK(false, specified<const Drived&&, const Base&>);
+//     MAGIC_CHECK(false, specified<Drived, const Base&>);
+//     MAGIC_CHECK(false, specified<const Drived, const Base&>);
+//     MAGIC_CHECK(false, specified<Drived&, const Base&>);
+//     MAGIC_CHECK(true, specified<const Drived&, const Base&>);
+//     MAGIC_CHECK(false, specified<Drived&&, const Base&>);
+//     MAGIC_CHECK(false, specified<const Drived&&, const Base&>);
 
-    MAGIC_CHECK(false, specified<Drived, Base&&>);
-    MAGIC_CHECK(false, specified<const Drived, Base&&>);
-    MAGIC_CHECK(true, specified<Drived&, Base&&>);
-    MAGIC_CHECK(true, specified<const Drived&, Base&&>);
-    MAGIC_CHECK(true, specified<Drived&&, Base&&>);
-    MAGIC_CHECK(true, specified<const Drived&&, Base&&>);
+//     MAGIC_CHECK(false, specified<Drived, Base&&>);
+//     MAGIC_CHECK(false, specified<const Drived, Base&&>);
+//     MAGIC_CHECK(true, specified<Drived&, Base&&>);
+//     MAGIC_CHECK(true, specified<const Drived&, Base&&>);
+//     MAGIC_CHECK(true, specified<Drived&&, Base&&>);
+//     MAGIC_CHECK(true, specified<const Drived&&, Base&&>);
 
-    MAGIC_CHECK(false, specified<Drived, const Base&&>);
-    MAGIC_CHECK(false, specified<const Drived, const Base&>);
-    MAGIC_CHECK(false, specified<Drived&, const Base&&>);
-    MAGIC_CHECK(true, specified<const Drived&, const Base&&>);
-    MAGIC_CHECK(false, specified<Drived&&, const Base&&>);
-    MAGIC_CHECK(true, specified<const Drived&&, const Base&&>);
-}
+//     MAGIC_CHECK(false, specified<Drived, const Base&&>);
+//     MAGIC_CHECK(false, specified<const Drived, const Base&>);
+//     MAGIC_CHECK(false, specified<Drived&, const Base&&>);
+//     MAGIC_CHECK(true, specified<const Drived&, const Base&&>);
+//     MAGIC_CHECK(false, specified<Drived&&, const Base&&>);
+//     MAGIC_CHECK(true, specified<const Drived&&, const Base&&>);
+// }
 
 TEST(general, fwd)
 {
