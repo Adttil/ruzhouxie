8e3563bbd226b139e23daf530e7b3eea23798b00
Adttil <1963096826@qq.com>
2024-08-14 04:52:31 +0800

    Add view;

--- include/ruzhouxie/view_interface.hpp
+++ include/ruzhouxie/view_interface.hpp
@@ -2,12 +2,13 @@
 #define RUZHOUXIE_VIEW_INTERFACE_HPP
 
 #include "general.hpp"
+#include "simplify.hpp"
 
 #include "macro_define.hpp"
 
 namespace rzx 
 {
-    template<typename View>
+    template<class View>
     struct view_interface
     {};
 
@@ -16,6 +17,62 @@ namespace rzx
     {
         { []<class V>(view_interface<V>&)->V*{}(t) } -> std::same_as<std::remove_cvref_t<T>*>;
     };
+
+    namespace detail
+    {
+        template<class T>
+        struct view_storage
+        {
+            T base;
+        };
+    }
+
+    template<class T>
+    struct view : detail::view_storage<T>, view_interface<view<T>>
+    {
+        template<size_t I, class Self>
+        constexpr decltype(auto) get(this Self&& self)
+        {
+            if constexpr(I < child_count<T>)
+            {
+                return FWD(self) | child<I>;
+            }
+            else
+            {
+                return end();
+            } 
+        }
+
+        template<auto Usage, auto Layout, class Self>
+        constexpr decltype(auto) simplify(this Self&& self)
+        {
+            return FWD(self, base) | rzx::simplify<Usage, Layout>;
+        }
+    };
+
+    template<class T>
+    view(T) -> view<T>;
+
+    namespace detail 
+    {
+        struct refer_t : adaptor_closure<refer_t>
+        {
+            template<class T>
+            constexpr decltype(auto) operator()(T&& t) const
+            {
+                if constexpr(std::is_reference_v<T>)
+                {
+                    return t;
+                }
+                else
+                {
+                    return view<T&&>{ FWD(t) };
+                }
+            }
+        };
+    }
+
+    inline constexpr detail::refer_t refer{};
 }
 
 #include "macro_undef.hpp"
